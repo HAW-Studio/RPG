c4059a4e01f4296cd3482d15d0e595c068f06635
Outflux7 <57254523+Outflux7@users.noreply.github.com>
2019-11-11 12:26:47 +0100

    Battlesystem Update

---
+++ RPG Battlesystem/Assets/Scenes/BattleScene/Battlescript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battlescript : MonoBehaviour
+{
+
+    //ATTRIBUTES
+
+    public GameObject player;
+
+    public int health = 20;
+    public int dmg = 5;
+
+    //FUNCTIONS
+
+    public void doDmg(Enemy enemy)
+    {
+        enemy.health -= dmg;
+    }
+
+    // OBJECTS
+
+    Enemy test = new Enemy("test", 20, 5);
+
+
+
+    //START
+
+    void Start()
+    {
+        test.Encounter();
+
+
+    }
+
+}
---
+++ RPG Battlesystem/Assets/Scenes/BattleScene/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+
+    //ATTRIBUTES
+
+    public GameObject enemy;
+    public GameObject messgControl;
+
+    public string name;
+    public int health;
+    public int dmg;
+
+    //CONSTRUCTOR
+
+    public Enemy(string name,int health_,int dmg_)
+    {
+        health = health_;
+        dmg = dmg_;
+    }
+
+    //FUNCTIONS
+
+    public void Encounter()
+    {
+        enemy.SetActive(true);
+        messgControl.GetComponent<Text>().text = "An " + name + " has appeared";
+
+    }
+
+    private void Awake()
+    {
+        enemy.SetActive(false);
+    }
+
+}
